96a5ba522795139f4d3a80d014cb22eea76f20aa
N0ah580 <145355575+N0ah580@users.noreply.github.com>
2023-12-01 09:19:04 -0500

    stupid

--- peggys.py
+++ peggys.py
@@ -1,3 +1,18 @@
-# Programmer: 
-# Description: 
+# Programmer: Noah
+# Description: Peggy's
 
+#Create a function to find the cost of Peggy's Pest Control
+def peggys_price (level, size):
+      if size < 0:
+          print ("Invalid house size")
+      elif level.lower () == "basic":
+        answer = size * 0.05
+        return (f"$ {answer:,.2f}")
+      elif level.lower () == "advanced":
+        answer = size * 0.10
+        return (f"${answer:,.2f}")
+      elif level.lower () == "complete":
+        answer = size * 0.20
+        return (f"${answer:,.2f}")
+      else:
+        print ("Invalid price level")
